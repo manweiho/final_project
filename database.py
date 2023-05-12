dbfb2474a4ef27e6d17fbdc2f00dc27665c0d118
manweiho <82921578+manweiho@users.noreply.github.com>
2023-05-12 18:37:46 -0400

    Update database.py

--- database.py
+++ database.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-conn = sqlite3.connect('/home/ubuntu/week13_flask/employees.db')
+conn = sqlite3.connect('/home/ubuntu/final_project/flights.db')
 print("Opened database successfully")
 
 conn.execute('CREATE TABLE employees (EmpID TEXT, EmpName TEXT, EmpGender TEXT, EmpPhone TEXT, EmpBdate TEXT)')
