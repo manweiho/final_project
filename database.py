1f81149914113661f8c3f8ffe5be4cd7e6372fcf
manweiho <82921578+manweiho@users.noreply.github.com>
2023-05-12 18:43:50 -0400

    Update database.py

--- database.py
+++ database.py
@@ -3,8 +3,7 @@ import sqlite3
 conn = sqlite3.connect('/home/ubuntu/final_project/flights.db')
 print("Opened database successfully")
 
-conn.execute('CREATE TABLE employees (EmpID TEXT, EmpName TEXT, EmpGender TEXT, EmpPhone TEXT, EmpBdate TEXT)')
+conn.execute('CREATE TABLE flights (FLIGHTID INT AUTO_INCREMENT PRIMARY KEY, FirstName TEXT, LastName TEXT, Email TEXT, Phone TEXT, Origins TEXT, Destination TEXT, Date DATE)')
 print("Table created successfully")
-
 
 conn.close(
