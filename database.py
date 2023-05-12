f361d6e5df173a66c62348c945a1da9989495b30
manweiho <82921578+manweiho@users.noreply.github.com>
2023-05-12 16:59:57 -0400

    Create database.py

---
+++ database.py
@@ -0,0 +1,10 @@
+import sqlite3
+
+conn = sqlite3.connect('/home/ubuntu/week13_flask/employees.db')
+print("Opened database successfully")
+
+conn.execute('CREATE TABLE employees (EmpID TEXT, EmpName TEXT, EmpGender TEXT, EmpPhone TEXT, EmpBdate TEXT)')
+print("Table created successfully")
+
+
+conn.close(
