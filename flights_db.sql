cca41a675c46fadd492415fbd45045e4f11a2333
manweiho <82921578+manweiho@users.noreply.github.com>
2023-05-17 21:05:56 -0400

    Create flights_db.sql

---
+++ flights_db.sql
@@ -0,0 +1,39 @@
+-- MySQL dump 10.13  Distrib 8.0.33, for Linux (x86_64)
+--
+-- Host: localhost    Database: flights_db
+-- ------------------------------------------------------
+-- Server version	8.0.33-0ubuntu0.22.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `flights`
+--
+
+DROP TABLE IF EXISTS `flights`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `flights` (
+  `FlightID` int NOT NULL AUTO_INCREMENT,
+  `FirstName` varchar(30) NOT NULL,
+  `LastName` varchar(30) DEFAULT NULL,
+  `Email` varchar(30) DEFAULT NULL,
+  `Phone` varchar(30) DEFAULT NULL,
+  `Origin` varchar(30) DEFAULT NULL,
+  `Destination` varchar(30) DEFAULT NULL,
+  `Date` date DEFAULT NULL,
+  `DepartureTime` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`FlightID`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
